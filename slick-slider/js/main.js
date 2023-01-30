a0845f2607f8c50ac422c011d8d1d51e7b481c69
StacyMch <greensummertea@gmail.com>
2023-01-30 04:31:50 +0300

    технические фиксы по слайдеру, картинки пока черновые, со стилизацией тоже потом наведем красоту, пока лишь бы работало

--- slick-slider/js/main.js
+++ slick-slider/js/main.js
@@ -2,13 +2,12 @@ $(document).ready(function(){
     $('.slider').slick({
         arrows: true, //отображение стрелок
         dots: true, //отображение точек
-        adaptiveHeight: true, //слайдер подстраивается по высоте под высоту активного слайда
-        slidesToShow: 4, //сколько слайдов будет показано в одном ряду
+        adaptiveHeight: false, //слайдер подстраивается по высоте под высоту активного слайда
+        slidesToShow: 2, //сколько слайдов будет показано в одном ряду
         slidesToScroll: 1, //сколько слайдов пролистывается по одному нажатию на стрелку
         speed: 1000, //скорость прокрутки (в мс)
         easing: 'linear', //тип анимации
         infinite: true, //будет ли слайдер пролистываться бесконечно
-        initialSlide: 0, //какой слайдер будет начальным
         autoplay: false, //автоматическое пролистывание
         autoplaySpeed: 1000, //интервал автопролистывания
         pauseOnFocus: true, //прекращение автопролистывания при focus
@@ -27,12 +26,7 @@ $(document).ready(function(){
         verticalSwiping: false, //переключение горизонтального свайпа на вертикальный
         responsive: [
             {
-                breakpoint: 768, //ширина окна, начиная с которой активируются новые значения тех свойств, которые перечислены выше (аналог @media в css)
-                settings: {
-                    slidesToShow: 2
-                }
-            }, {
-                breakpoint: 480,
+                breakpoint: 1020,
                 settings: {
                     slidesToShow: 1
                 }
