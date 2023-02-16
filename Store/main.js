873bb7ff46310396b2051d32d91abfbbc3303149
Rodion-de-Front <kalinichev06@bk.ru>
2023-02-16 21:55:30 +0300

    отзывы

--- Store/main.js
+++ Store/main.js
@@ -4,22 +4,24 @@ function sendRequest() {
 
     let msg = document.getElementById('msg').value;
 
-    let tmpl_mes = document.getElementById('tmpl_mes');
+    let array = Array.from(msg);
 
-    // создаём переменную с текущем временем, датой, месяцем, годом
-    let today = new Date();
+    console.log(array);
 
-    // создаём переменную, куда записываем только время и отрезаем секунды
-    let now = today.toLocaleTimeString().slice(0,-3);
-
-    container+= tmpl_mes.innerHTML.replace('${message}', msg)
-                                  .replace('${date}', now);
-
+    let tmpl_card = document.getElementById('tmpl_card');
+        
+    container+= tmpl_card.innerHTML.replace('${message}', msg);
+        
     console.log(container);
+        
+    document.getElementById('box__body').innerHTML = container;    
+            
+    document.getElementById('msg').value = '';
 
-    document.getElementById('chat-box__body').innerHTML = container;     
-    
 }
+
+
+
 
 
 
